560d243b5c437a43a0d5f698ca22e61bd893958c
shaik-md-musayyeb <smd52952@gmail.com>
2020-02-24 22:35:21 +0530

    modified

--- app.js
+++ app.js
@@ -121,6 +121,8 @@ else
 // Generate 3 paragraphs. Store the text in a variable type of string.
 // Make your program count the number of words in the string.
 // Make your program count the number of times the Latin word et appears.
+
+
 // Bonus 2:
 // Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
 
